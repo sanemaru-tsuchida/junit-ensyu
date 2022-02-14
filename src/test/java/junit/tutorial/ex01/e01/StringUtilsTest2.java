2e8d4af1c8c1db046b4d518f1f06bf5de7f4b05d
TsuchidaSanemaru <sanemaru.tsuchida@rakus-partners.co.jp>
2022-02-14 10:35:05 +0900

    1-1 完了

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest2.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest2.java
@@ -3,16 +3,18 @@ package junit.tutorial.ex01.e01;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class StringUtilsTest2 {
-
+	
 
 	@Test
 	void test() {
-		assertEquals("aaa", StringUtils.toSnakeCase("aaa"));
+		assertEquals("aaa", StringUtils.toSnakeCase("aaa") ," StringUtilsのtoSnakeCaseテスト");
+		assertEquals("hello_world", StringUtils.toSnakeCase("HelloWorld") ," StringUtilsのtoSnakeCaseテスト");
+		assertEquals("practice_junit", StringUtils.toSnakeCase("practiceJunit") ," StringUtilsのtoSnakeCaseテスト");
+		
 	}
 
 }
